a3cea885fc6911cf08a6fc5400ac315596997b30
evangallagher <evga7768@colorado.edu>
2019-09-13 23:13:58 -0600

    Final commit of basic test shell and README

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# best-practices
+# Folder containing assignment 2 in software engineering for scientists: best practices. 
--- basics_test.sh
+++ basics_test.sh
@@ -1,16 +1,21 @@
+#!/bin/bash
+
 pycodestyle style.py
 
 pycodestyle get_column_stats.py
 
-(for i in `seq 1 100`; do 
+(for i in `seq 1 100`; do
     echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
 done )> data.txt
 
 python get_column_stats.py data.txt 2
 
+stdev(col_num)
+avg(col_num)
+
 
 V=1
-(for i in `seq 1 100`; do 
+(for i in `seq 1 100`; do
     echo -e "$V\t$V\t$V\t$V\t$V";
 done )> data.txt
 
